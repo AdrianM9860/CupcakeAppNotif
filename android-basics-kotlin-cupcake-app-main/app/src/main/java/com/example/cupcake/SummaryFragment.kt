2292ea9de49b21c563d53d3421f8334b270526b8
AdrianM9860 <60239066+AdrianM9860@users.noreply.github.com>
2021-03-19 14:31:21 -0700

    Update SummaryFragment.kt

--- android-basics-kotlin-cupcake-app-main/app/src/main/java/com/example/cupcake/SummaryFragment.kt
+++ android-basics-kotlin-cupcake-app-main/app/src/main/java/com/example/cupcake/SummaryFragment.kt
@@ -15,55 +15,108 @@
  */
 package com.example.cupcake
 
-import android.app.NotificationManager
-import android.app.PendingIntent
-import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.NotificationCompat
-import androidx.navigation.NavController
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.setupActionBarWithNavController
+import android.os.CountDownTimer
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
+import com.example.cupcake.databinding.FragmentSummaryBinding
+import com.example.cupcake.model.OrderViewModel
 
 /**
- * Activity for cupcake order flow.
+ * [SummaryFragment] contains a summary of the order details with a button to share the order
+ * via another app.
  */
-class MainActivity : AppCompatActivity(R.layout.activity_main) {
+val activity: MainActivity = MainActivity()
+class SummaryFragment : Fragment() {
 
-    private lateinit var navController: NavController
+    // Binding object instance corresponding to the fragment_summary.xml layout
+    // This property is non-null between the onCreateView() and onDestroyView() lifecycle callbacks,
+    // when the view hierarchy is attached to the fragment.
+    private var binding: FragmentSummaryBinding? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    // Use the 'by activityViewModels()' Kotlin property delegate from the fragment-ktx artifact
+    private val sharedViewModel: OrderViewModel by activityViewModels()
 
-        // Retrieve NavController from the NavHostFragment
-        val navHostFragment = supportFragmentManager
-                .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        navController = navHostFragment.navController
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val fragmentBinding = FragmentSummaryBinding.inflate(inflater, container, false)
+        binding = fragmentBinding
+        return fragmentBinding.root
+    }
 
-        // Set up the action bar for use with the NavController
-        setupActionBarWithNavController(navController)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding?.apply {
+            lifecycleOwner = viewLifecycleOwner
+            viewModel = sharedViewModel
+            summaryFragment = this@SummaryFragment
+        }
     }
 
     /**
-     * Handle navigation when the user chooses Up from the action bar.
+     * Submit the order by sharing out the order details to another app via an implicit intent.
      */
-    override fun onSupportNavigateUp(): Boolean {
-        return navController.navigateUp() || super.onSupportNavigateUp()
+    fun sendOrder() {
+        // Construct the order summary text with information from the view model
+        val numberOfCupcakes = sharedViewModel.quantity.value ?: 0
+        val orderSummary = getString(
+            R.string.order_details,
+            resources.getQuantityString(R.plurals.cupcakes, numberOfCupcakes, numberOfCupcakes),
+            sharedViewModel.flavor.value.toString(),
+            sharedViewModel.date.value.toString(),
+            sharedViewModel.price.value.toString()
+        )
+
+        // Create an ACTION_SEND implicit intent with order details in the intent extras
+        val intent = Intent(Intent.ACTION_SEND)
+            .setType("text/plain")
+            .putExtra(Intent.EXTRA_SUBJECT, getString(R.string.new_cupcake_order))
+            .putExtra(Intent.EXTRA_TEXT, orderSummary)
+
+        // Check if there's an app that can handle this intent before launching it
+        if (activity?.packageManager?.resolveActivity(intent, 0) != null) {
+            // Start a new activity with the given intent (this may open the share dialog on a
+            // device if multiple apps can handle this intent)
+            startActivity(intent)
+        }
+        timer.start()
     }
-    fun addNotification() {
-        var builder =  NotificationCompat.Builder(this, 0.toString())
-            .setSmallIcon(R.drawable.cupcake)
-            .setContentTitle("Its done!")
-            .setContentText("Your order is ready! Come pick it up!")
 
-        val notificationIntent = Intent(this, SummaryFragment::class.java)
-        val contentIntent: PendingIntent = PendingIntent.getActivity(this, 0, notificationIntent,
-            0)
-        builder.setContentIntent(contentIntent)
+    /**
+     * Cancel the order and start over.
+     */
+    fun cancelOrder() {
+        // Reset order in view model
+        sharedViewModel.resetOrder()
 
-        //Add as a notification
-        val manager: NotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        manager.notify(0, builder.build())
+        // Navigate back to the [StartFragment] to start over
+        findNavController().navigate(R.id.action_summaryFragment_to_startFragment)
+    }
+
+    /**
+     * This fragment lifecycle method is called when the view hierarchy associated with the fragment
+     * is being removed. As a result, clear out the binding object.
+     */
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
+    }
+}
+private val timer = object : CountDownTimer(120000, 1000) {
+    override fun onTick(millisUntilFinished: Long) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onFinish() {
+        // Send the notification
+        activity.addNotification()
     }
 }
